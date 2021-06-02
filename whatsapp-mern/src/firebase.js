131c0fd20b0c5f41bf89d89a26fc8d84884251da
sairam18814 <74001231+sairam18814@users.noreply.github.com>
2021-06-02 19:19:49 +0530

    sixth commit

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB1T0GyvZH17wraeTZGkuS972a9SosNDrA",
+    authDomain: "whatsapp-clone-f9fbb.firebaseapp.com",
+    projectId: "whatsapp-clone-f9fbb",
+    storageBucket: "whatsapp-clone-f9fbb.appspot.com",
+    messagingSenderId: "726097200698",
+    appId: "1:726097200698:web:10b0ba848d855595b7d703",
+    measurementId: "G-3KCMB5GCCC"
+  };
