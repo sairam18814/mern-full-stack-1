84add2c37270f04facc1343d56cb849e33de813d
sairam18814 <74001231+sairam18814@users.noreply.github.com>
2021-06-12 20:06:46 +0530

    eight commit

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import "./Sidebar.css"
 
 function Sidebar() {
     return (
         <div>
-            
+            <h1>I am sidebar</h1>
+            <div className="sidebar_header">
+                
+                <div className="sidebar_headerright">
+
+                </div>
+            </div>
         </div>
     )
 }
