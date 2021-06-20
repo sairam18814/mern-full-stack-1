24764c26b597b8cc1b898a987f81493e838aa98f
sairam18814 <74001231+sairam18814@users.noreply.github.com>
2021-06-20 21:34:55 +0530

    twelth commit

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am sidebar</h1>
-            <div className="sidebar_header">
-                
-                <div className="sidebar_headerRight">
 
+            <div className="sidebar_header">
+
+                <Avatar src=""/>
+
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
                 </div>
+
             </div>
+
+            <div className="sidebar_search">
+
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+
+            </div>
+
+            <div className="sidebar_chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+            </div>
+
         </div>
     )
 }
